ccde3250e117fe435d7fdaa5974826f97aa3950f
Zak <zakary.howard94@gmail.com>
2018-12-05 09:52:12 +0100

    get request set up and website is receiving data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { BeersService } from './beers.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'punk-api';
+
+  constructor(private _beerService: BeersService) {
+    
+  }
 }
--- src/app/beers.service.ts
+++ src/app/beers.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class BeersService {
+  beers: Object;
+  beerUrl = 'https://api.punkapi.com/v2/beers/';
 
-  constructor() { }
+  constructor(private _httpClient: HttpClient) { 
+    this.getListOfBeer()
+  }
+
+  getListOfBeer() {
+    return this._httpClient.get(this.beerUrl).subscribe((res) => {
+      debugger;
+      this.beers = res;
+    });
+  }
 }
