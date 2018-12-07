ad3828283921288b0e41ca61647a982c70feda09
Zak <zakary.howard94@gmail.com>
2018-12-07 19:55:22 +0100

    created placeholder buttons to show more or less beers

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 import { BeersListComponent } from './beers-list/beers-list.component';
+import { BeersListControlsComponent } from './beers-list-controls/beers-list-controls.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    BeersListComponent
+    BeersListComponent,
+    BeersListControlsComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/beers-list-controls/beers-list-controls.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: '[app-beers-list-controls]',
+  templateUrl: './beers-list-controls.component.html',
+  styleUrls: ['./beers-list-controls.component.scss']
+})
+export class BeersListControlsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
