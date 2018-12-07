5e76c899fd2043b42328e840cdbcc53e5f443715
Zak <zakary.howard94@gmail.com>
2018-12-07 17:33:22 +0100

    removed commented text

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,35 +13,3 @@ export class AppComponent {
     _beerService.getListOfBeer().subscribe(res => this.beers = res)
   }
 }
-
-// import { Component } from '@angular/core';
-// import { BeersService } from './beers.service';
-// import { HttpClient } from '@angular/common/http';
-
-// @Component({
-//   selector: 'app-root',
-//   templateUrl: './app.component.html',
-//   styleUrls: ['./app.component.css']
-// })
-// export class AppComponent {
-//   title;
-
-//   constructor(private http: HttpClient) {
-//     // this.title = _beerService.getListOfBeers();
-//   }
-
-//   ngOnInit(): void {
-//     this.http.get('https://api.punkapi.com/v2/beers')
-//       .subscribe(data => {
-//         this.title = data[5]["name"];
-//       },
-//         (error) => {
-//           debugger;
-//           console.log(error)
-//         }
-//       );
-//   }
-//   myFunction() {
-//     console.log(this.title)
-//   }
-// }
