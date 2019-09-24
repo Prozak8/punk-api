c494a729e8871259819ee12762b7567e27c69b8e
Zak <zakary.howard94@gmail.com>
2019-09-24 08:58:31 +0200

    app working again

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,22 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 import { BeersListComponent } from './beers-list/beers-list.component';
-import { BeersListControlsComponent } from './beers-list-controls/beers-list-controls.component';
 import { BeerShowComponent } from './beer-show/beer-show.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    BeersListComponent,
-    BeersListControlsComponent,
-    BeerShowComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    HttpClientModule
-  ],
+  declarations: [AppComponent, BeersListComponent, BeerShowComponent],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
